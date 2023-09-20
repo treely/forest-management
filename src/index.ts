74f5feb3aff4268fec22f78c935e7b8b0990244b
Raul Steurer <raul@steurer.io>
2023-09-20 17:06:55 +0200

    feat: Export yieldTables

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,7 @@ export * from '@/components/Forestry';
 export * from '@/components/OperationClass';
 export * from '@/components/OperationClasses';
 
+export { default as yieldTables } from './constants/yieldTables';
+
 export type { default as FeasibilityStudyConfig } from '@/models/FeasibilityStudyConfig';
+export type { default as YieldTableEnum } from '@/models/YieldTableEnum';
