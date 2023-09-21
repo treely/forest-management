135bcef25f4f792beee0db82b037c11759910978
Raul Steurer <raul@steurer.io>
2023-09-21 12:10:46 +0200

    fix: Export YieldTableEnum as enum, not as type

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,6 @@ export * from '@/components/OperationClasses';
 
 export { default as yieldTables } from './constants/yieldTables';
 
+export { default as YieldTableEnum } from '@/models/YieldTableEnum';
+
 export type { default as FeasibilityStudyConfig } from '@/models/FeasibilityStudyConfig';
-export type { default as YieldTableEnum } from '@/models/YieldTableEnum';
