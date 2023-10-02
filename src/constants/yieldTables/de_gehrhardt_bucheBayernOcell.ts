f5b12525a6d0ae940f9865f61f7a225b17d97877
Erikkue <erikkuegler@gmx.de>
2023-10-02 13:44:26 +0200

    feat: create new yieldTable buche bayern

---
+++ src/constants/yieldTables/de_gehrhardt_bucheBayernOcell.ts
@@ -0,0 +1,315 @@
+import YieldTable from '../../models/YieldTable';
+
+const de_gehrhardt_bucheBayernOcell: YieldTable = {
+  meta: {
+    title: 'Buche Bayern',
+    author: 'E. Gehrhardt',
+    copyright: '',
+    source: '',
+    sourceLink: '',
+  },
+  rows: [
+    {
+      yieldClass: 1.0,
+      age: 10.0,
+      stock: 0.0,
+    },
+    {
+      yieldClass: 1.0,
+      age: 20.0,
+      stock: 9.0,
+    },
+    {
+      yieldClass: 1.0,
+      age: 30.0,
+      stock: 63.0,
+    },
+    {
+      yieldClass: 1.0,
+      age: 40.0,
+      stock: 157.0,
+    },
+    {
+      yieldClass: 1.0,
+      age: 50.0,
+      stock: 246.0,
+    },
+    {
+      yieldClass: 1.0,
+      age: 60.0,
+      stock: 324.0,
+    },
+    {
+      yieldClass: 1.0,
+      age: 70.0,
+      stock: 392.0,
+    },
+    {
+      yieldClass: 1.0,
+      age: 80.0,
+      stock: 452.0,
+    },
+    {
+      yieldClass: 1.0,
+      age: 90.0,
+      stock: 504.0,
+    },
+    {
+      yieldClass: 1.0,
+      age: 100.0,
+      stock: 549.0,
+    },
+    {
+      yieldClass: 1.0,
+      age: 110.0,
+      stock: 588.0,
+    },
+    {
+      yieldClass: 1.0,
+      age: 120.0,
+      stock: 622.0,
+    },
+    {
+      yieldClass: 2.0,
+      age: 10.0,
+      stock: 0.0,
+    },
+    {
+      yieldClass: 2.0,
+      age: 20.0,
+      stock: 5.0,
+    },
+    {
+      yieldClass: 2.0,
+      age: 30.0,
+      stock: 38.0,
+    },
+    {
+      yieldClass: 2.0,
+      age: 40.0,
+      stock: 113.0,
+    },
+    {
+      yieldClass: 2.0,
+      age: 50.0,
+      stock: 191.0,
+    },
+    {
+      yieldClass: 2.0,
+      age: 60.0,
+      stock: 260.0,
+    },
+    {
+      yieldClass: 2.0,
+      age: 70.0,
+      stock: 321.0,
+    },
+    {
+      yieldClass: 2.0,
+      age: 80.0,
+      stock: 374.0,
+    },
+    {
+      yieldClass: 2.0,
+      age: 90.0,
+      stock: 420.0,
+    },
+    {
+      yieldClass: 2.0,
+      age: 100.0,
+      stock: 460.0,
+    },
+    {
+      yieldClass: 2.0,
+      age: 110.0,
+      stock: 494.0,
+    },
+    {
+      yieldClass: 2.0,
+      age: 120.0,
+      stock: 523.0,
+    },
+    {
+      yieldClass: 3.0,
+      age: 10.0,
+      stock: 0.0,
+    },
+    {
+      yieldClass: 3.0,
+      age: 20.0,
+      stock: 2.0,
+    },
+    {
+      yieldClass: 3.0,
+      age: 30.0,
+      stock: 20.0,
+    },
+    {
+      yieldClass: 3.0,
+      age: 40.0,
+      stock: 75.0,
+    },
+    {
+      yieldClass: 3.0,
+      age: 50.0,
+      stock: 141.0,
+    },
+    {
+      yieldClass: 3.0,
+      age: 60.0,
+      stock: 202.0,
+    },
+    {
+      yieldClass: 3.0,
+      age: 70.0,
+      stock: 256.0,
+    },
+    {
+      yieldClass: 3.0,
+      age: 80.0,
+      stock: 302.0,
+    },
+    {
+      yieldClass: 3.0,
+      age: 90.0,
+      stock: 342.0,
+    },
+    {
+      yieldClass: 3.0,
+      age: 100.0,
+      stock: 377.0,
+    },
+    {
+      yieldClass: 3.0,
+      age: 110.0,
+      stock: 407.0,
+    },
+    {
+      yieldClass: 3.0,
+      age: 120.0,
+      stock: 432.0,
+    },
+    {
+      yieldClass: 4.0,
+      age: 10.0,
+      stock: 0.0,
+    },
+    {
+      yieldClass: 4.0,
+      age: 20.0,
+      stock: 0.0,
+    },
+    {
+      yieldClass: 4.0,
+      age: 30.0,
+      stock: 8.0,
+    },
+    {
+      yieldClass: 4.0,
+      age: 40.0,
+      stock: 38.0,
+    },
+    {
+      yieldClass: 4.0,
+      age: 50.0,
+      stock: 87.0,
+    },
+    {
+      yieldClass: 4.0,
+      age: 60.0,
+      stock: 140.0,
+    },
+    {
+      yieldClass: 4.0,
+      age: 70.0,
+      stock: 191.0,
+    },
+    {
+      yieldClass: 4.0,
+      age: 80.0,
+      stock: 237.0,
+    },
+    {
+      yieldClass: 4.0,
+      age: 90.0,
+      stock: 278.0,
+    },
+    {
+      yieldClass: 4.0,
+      age: 100.0,
+      stock: 313.0,
+    },
+    {
+      yieldClass: 4.0,
+      age: 110.0,
+      stock: 344.0,
+    },
+    {
+      yieldClass: 4.0,
+      age: 120.0,
+      stock: 370.0,
+    },
+    {
+      yieldClass: 5.0,
+      age: 10.0,
+      stock: 0.0,
+    },
+    {
+      yieldClass: 5.0,
+      age: 20.0,
+      stock: 0.0,
+    },
+    {
+      yieldClass: 5.0,
+      age: 30.0,
+      stock: 1.0,
+    },
+    {
+      yieldClass: 5.0,
+      age: 40.0,
+      stock: 13.0,
+    },
+    {
+      yieldClass: 5.0,
+      age: 50.0,
+      stock: 41.0,
+    },
+    {
+      yieldClass: 5.0,
+      age: 60.0,
+      stock: 81.0,
+    },
+    {
+      yieldClass: 5.0,
+      age: 70.0,
+      stock: 124.0,
+    },
+    {
+      yieldClass: 5.0,
+      age: 80.0,
+      stock: 165.0,
+    },
+    {
+      yieldClass: 5.0,
+      age: 90.0,
+      stock: 202.0,
+    },
+    {
+      yieldClass: 5.0,
+      age: 100.0,
+      stock: 234.0,
+    },
+    {
+      yieldClass: 5.0,
+      age: 110.0,
+      stock: 261.0,
+    },
+    {
+      yieldClass: 5.0,
+      age: 120.0,
+      stock: 283.0,
+    },
+  ],
+};
+
+export default de_gehrhardt_bucheBayernOcell;
