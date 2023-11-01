f02717a0eb0dbf01905afd7bbdc05bfbd01de54d
Erikkue <erikkuegler@gmx.de>
2023-11-01 11:41:44 +0100

    feat: create new yield table fichte weitra

--- src/models/YieldTableEnum.ts
+++ src/models/YieldTableEnum.ts
@@ -13,6 +13,7 @@ enum YieldTableEnum {
   // AT Marschall
   AT_MARSCHALL_FichteBruck = 'at_marschall_fichteBruck',
   AT_MARSCHALL_FichteHochgebirge = 'at_marschall_fichteHochgebirge',
+  AT_MARSCHALL_FichteWeitra = 'at_marschall_fichteWeitra',
   AT_MARSCHALL_KieferLitschau = 'at_marschall_kieferLitschau',
 
   // CH Marschall
